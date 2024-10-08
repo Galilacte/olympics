eaa797248a428b49d6597de533a4326265857dfc
florent Badin <florent.badin1@gmail.com>
2024-10-08 10:26:23 +0200

    ajout des tests sur main avec 100% valide

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,34 @@
 from olympics.__main__ import main
+import pytest
+import argparse
 
 
+
+
+
+    
+    
+    
 def test_countries():
     argv = ['countries']
     main(argv)
+    
+    
+def test_collective():
+    argv = ['collective']
+    main(argv)
+    
+    
+def test_individual():
+    argv = ['individual']
+    main(argv)
+    
+    
+def test_main():
+    with pytest.raises(argparse.ArgumentTypeError):
+        argv = ['countries', '--top', '0']
+        main(argv)
+    
+    
+
+    
